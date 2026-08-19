bfcb2bd5ccf5f2d7b8125e74ecb8aa9b7ba45dfb
PIOUS22 <110350187+PIOUS22@users.noreply.github.com>
2026-08-19 11:11:12 +0600

    Add SupplierScreen with placeholder text

---
+++ v2/lib/screens/supplier_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class SupplierScreen extends StatelessWidget {
+  const SupplierScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        'Supplier Management\nComing next',
+        textAlign: TextAlign.center,
+        style: TextStyle(fontSize: 22),
+      ),
+    );
+  }
+}
