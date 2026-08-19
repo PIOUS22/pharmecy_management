6dfa4193a79f0de8a95579f0af93018bda5e15ce
PIOUS22 <110350187+PIOUS22@users.noreply.github.com>
2026-08-19 13:36:20 +0600

    Fix import statement for supplier screen

--- v2/lib/screens/dashboard_screen.dart
+++ v2/lib/screens/dashboard_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'suppliers_screen.dart';
+import 'supplier_screen.dart';
 
 import '../services/auth_service.dart';
 import 'medicines_screen.dart';
