e98332f9191a5b6c32214773047c0aa283a4f2fe
PIOUS22 <110350187+PIOUS22@users.noreply.github.com>
2026-08-19 11:10:39 +0600

    Add PurchaseScreen widget with placeholder text

---
+++ v2/lib/screens/purchase_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class PurchaseScreen extends StatelessWidget {
+  const PurchaseScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        'Purchase Management\nComing next',
+        textAlign: TextAlign.center,
+        style: TextStyle(fontSize: 22),
+      ),
+    );
+  }
+}
