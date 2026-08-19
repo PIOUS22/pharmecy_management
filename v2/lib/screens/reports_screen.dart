03782c1eda3eb98c9eed54c350ef1f2bff0a389a
PIOUS22 <110350187+PIOUS22@users.noreply.github.com>
2026-08-19 11:13:40 +0600

    Add ReportsScreen widget with placeholder text

---
+++ v2/lib/screens/reports_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ReportsScreen extends StatelessWidget {
+  const ReportsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        'Reports\nComing next',
+        textAlign: TextAlign.center,
+        style: TextStyle(fontSize: 22),
+      ),
+    );
+  }
+}
